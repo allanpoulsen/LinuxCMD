8e4396134d59cee90d9a356ffc0fed6a4f0c1a20
root <root@dockerprod.lan.local>
2019-05-03 10:10:29 +0000

    HEADER
    DESC

--- DockerScripts/observiumstart.sh
+++ DockerScripts/observiumstart.sh
@@ -7,13 +7,22 @@
 #		zuhkov/observium
 
 
-docker run -d \
-    --name observium \
-    -p 8000:8000 \
-    -v /nas2/docker/containers/observium/volumes/config:/config \
-    -v /nas2/docker/containers/observium/volumes/html:/opt/observium/html \
-    -v /nas2/docker/containers/observium/volumes/logs:/opt/observium/logs \
-    -v /nas2/docker/containers/observium/volumes/rrd:/opt/observium/rrd \
-    -v /nas2/docker/containers/observium/config.php:/config/config.php:ro \
-    -e TZ="Europe/Copenhagen" \
-    yelp/observium
+# docker run -d \
+#    --name observium \
+#    -p 8000:8000 \
+#    -v /nas2/docker/containers/observium/volumes/config:/config \
+#    -v /nas2/docker/containers/observium/volumes/html:/opt/observium/html \
+#    -v /nas2/docker/containers/observium/volumes/logs:/opt/observium/logs \
+#    -v /nas2/docker/containers/observium/volumes/rrd:/opt/observium/rrd \
+#    -v /nas2/docker/containers/observium/config.php:/config/config.php:ro \
+#    -e TZ="Europe/Copenhagen" \
+#    yelp/observium
+
+sudo docker run -d \
+           --name observium2 \
+           -v /nas2/docker/containers/observium2/volumes/config:/config \
+           -v /nas2/docker/containers/observium2/volumes/logs:/opt/observium/logs \
+           -v /nas2/docker/containers/observium2/volumes/rrd:/opt/observium/rrd \
+           -e TZ="Europe/Copenhagen" \
+           -p 8668:8668 \
+           uberchuckie/observium
