3ea5e79cd30311c9ae93efb7c6218cd102aa7908
root <root@dockerprod.lan.local>
2019-05-30 06:13:59 +0000

    SQL
    MY SQL and PHP admin initial setup - 2.try

---
+++ DockerScripts/mysqlstop.sh
@@ -0,0 +1,1 @@
+sudo docker run --name mysql_grejpriser -v /nas2/docker/containers/MySQL_GrejPriser:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Trvb6#fhsjj/thh -p 3306:3306 -d mysql:latest
---
+++ DockerScripts/phpmyadminstart.sh
@@ -0,0 +1,1 @@
+docker run --name myadmin -d -e PMA_HOST=mysql_grejpriser -p 81:80 phpmyadmin/phpmyadmin
