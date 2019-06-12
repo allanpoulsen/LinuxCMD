9f757800578505a4deaf9b8c9df357768737afc5
root <root@ubuntodocker01.mshome.net>
2019-06-12 18:35:19 +0000

    MySQL changes
    MySQL changes

--- DockerScripts/mysqlstart.sh
+++ DockerScripts/mysqlstart.sh
@@ -1,1 +1,6 @@
-sudo docker run --name mysql_grejpriser -v /nas2/docker/containers/MySQL_GrejPriser:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Trvb6#fhsjj/thh -p 3306:3306 -d mysql:latest
+# Initiel network creation for phpmyadmin
+# docker network create mysql-network
+
+# Specific mysql container, since :latest does not allow phpmyadmin to login ( without design changes ) - works out of the box
+
+sudo docker run --net=mysql-network --name mysql_grejpriser -v ~/containers/MySQL_GrejPriser:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Trvb6#fhsjj/thh -p 3306:3306 -d mysql:5.7.24
