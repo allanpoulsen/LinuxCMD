0e320982e0dfc3bfac7917bc2c7e7a41d399184f
Allan Poulsen <allanpoulsen@gmail.com>
2020-03-26 11:50:53 +0000

    Update Gitlab autologin.sh

--- Gitlab autologin.sh
+++ Gitlab autologin.sh
@@ -1,4 +1,12 @@
-´´´´
+# Inspiration: https://linuxize.com/post/using-the-ssh-config-file/
+# Inspiration: https://stackoverflow.com/questions/40427498/getting-permission-denied-public-key-on-gitlab
+
+ssh-keygen
+
+# Copy/paste public key to Gitlab
+# Choosen filename should be entered below ( std: id_rsa )
+
+nano ~/.ssh/config
 User git
    IdentityFile ~/.ssh/id_rsa
    TCPKeepAlive yes
@@ -6,4 +14,7 @@ User git
 
 Host dockerprod
    Port 2222
-´´´´
+
+# Example on clonening - WITHOUT sudo!!!
+git clone git@dockerprod:friluftsgrejpriser/scraperwin32.git
+git pull
